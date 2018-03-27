fc31ad6c5da4fe113de7a90bed73c38b181d3c29
Alec Roques <alec.roques@gmail.com>
2018-03-27 18:56:05 -0500

    Import Robot class

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import cozmo
+from cozmo.robot import Robot
 from cozmo_taste_game import get_food
 from cozmo_taste_game import Taster
 
 
-def cozmo_program(robot: cozmo.robot.Robot):
+def cozmo_program(robot: Robot):
     taster = Taster(robot)
     taster.get_new_preferences()
     # This is where Cozmo will try to recognize what food is in front of him
