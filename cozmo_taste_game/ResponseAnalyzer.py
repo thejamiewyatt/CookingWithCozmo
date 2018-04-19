dbbb5446164ef1585390921fcdd02980d930ec02
Alec Roques <alec.roques@gmail.com>
2018-04-19 10:28:16 -0500

    Reformat to PEP8 standards

--- cozmo_taste_game/ResponseAnalyzer.py
+++ cozmo_taste_game/ResponseAnalyzer.py
@@ -1,25 +1,26 @@
-# Class responsible for analyzing the json returned from the server
-# At the moment, getFoundFood() will only return a valid value if the same
-# Prop has been seen 3 times in a row with a confidence > .7
+"""Class responsible for analyzing the json returned from the server
+At the moment, getFoundFood() will only return a valid value if the same
+Prop has been seen 3 times in a row with a confidence > .7
+"""
 
-class ResponseAnalyzer():
 
-    def __init__(self, threshold, streakThreshold):
+class ResponseAnalyzer:
+    def __init__(self, threshold, streak_threshold):
         self.streak = 0
         self.streakFood = None
         self.identifiedFood = None
-        self.hasBeenChecked = True
+        self.has_been_checked = True
         self.threshold = threshold
-        self.streakThreashold = streakThreshold
-        
+        self.streak_threshold = streak_threshold
+
     # input: response json
-    def analyzeResponse(self, response):
-        self.hasBeenChecked = False
-        highestConfidence = 0.0
-        highestEntry = ''
+    def analyze_response(self, response):
+        self.has_been_checked = False
+        highest_confidence = 0.0
+        highest_entry = ''
 
         entries = {}
-        
+
         for key in response.keys():
             if key == "answer":
                 for guess in response[key].keys():
@@ -27,24 +28,23 @@ class ResponseAnalyzer():
                     entries[response[key][guess]] = guess
         for key in entries.keys():
             print(f'{entries[key]}: {key}')
-            if key > highestConfidence:
-                highestConfidence = key
-                highestEntry = entries[key]
+            if key > highest_confidence:
+                highest_confidence = key
+                highest_entry = entries[key]
 
-        
-        if highestConfidence > self.threshold:
-            if(highestEntry != 'garbage' and highestEntry == self.streakFood):
+        if highest_confidence > self.threshold:
+            if highest_entry != 'garbage' and highest_entry == self.streakFood:
                 self.streak += 1
-                if(self.streak >= self.streakThreashold):
+                if self.streak >= self.streak_threshold:
                     self.identifiedFood = self.streakFood
                     self.streak = 0
             else:
-                self.streakFood = highestEntry
+                self.streakFood = highest_entry
                 self.streak = 0
 
     def getFoundFood(self):
-        self.hasbeenChecked = True
-        if(self.identifiedFood != None):
+        self.has_been_checked = True
+        if self.identifiedFood is not None:
             food = self.identifiedFood
             self.identifiedFood = None
             return food
@@ -52,11 +52,9 @@ class ResponseAnalyzer():
             # Occurs when no valid food has been identified
             raise Exception("")
 
-    def hasFoundFood(self):
-        self.hasBeenChecked = True
-        if(self.identifiedFood != None):
+    def has_found_food(self):
+        self.has_been_checked = True
+        if self.identifiedFood is not None:
             return True
         else:
             return False
-
-
